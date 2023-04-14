60c115330b409795babdcf948f3222dd2b2763fb
Alina N <my_work_space@bk.ru>
2023-04-14 22:12:17 +0300

    first commit veding machine

--- seminar_1/src/Main.java
+++ seminar_1/src/Main.java
@@ -1,5 +1,26 @@
+import Products.BottleOfWater;
+import Products.Product;
+import VendingMachines.VendingMachine;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+
+        Product item1 = new Product("Cola",88.0);
+        item1.setPrice(98.0);
+
+        VendingMachine itemMachine = new VendingMachine(300);
+        itemMachine.addProduct(item1);
+        itemMachine.addProduct(new Product("Chips", 60.0));
+        itemMachine.addProduct(new Product("Butter", 105.0));
+        itemMachine.addProduct(new Product("Bread", 45.0));
+        itemMachine.addProduct(new Product("Snack", 56.0));
+        itemMachine.addProduct(new BottleOfWater("Cola", 88.0, 500 ));
+        itemMachine.addProduct(new BottleOfWater("Water", 188.0, 1500 ));        for (Product prod: itemMachine.getProductAll()) {
+            System.out.println(prod.toString());
+        }
+
+
+
+        System.out.println(item1.toString());
     }
 }
---
+++ seminar_1/src/VendingMachines/VendingMachine.java
@@ -0,0 +1,39 @@
+package VendingMachines;
+
+import Products.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VendingMachine {
+    private int volume;
+    private List<Product> products;
+    private List<String> workLog;
+
+    public VendingMachine(int volume){
+        this.volume = volume;
+        products = new ArrayList<Product>();
+        workLog = new ArrayList<String>();
+    }
+    public void addProduct(Product prod) {
+        products.add(prod);
+    }
+
+    public void addSales(String line) {
+        workLog.add(line);
+    }
+
+    public Product getProdByName(String name) {
+        for (Product product: products) {
+            if (product.getName().contains(name))
+                return product;
+        }
+        return null;
+    }
+
+    public List<Product> getProductAll() {
+        return products;
+    }
+
+
+}
